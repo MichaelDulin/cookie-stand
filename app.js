b8dda9530a4c4e0c226e0053ae07f40bff057e72
Michael Dulin <michaeldulin1211@gmail.com>
2023-01-30 13:14:46 -0800

    Implemented 5 city obejcts

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let seattleStore = {
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  getNumberOfRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  }
+}
+
+let tokyoStore = {
+  min: 3,
+  max: 24,
+  avg: 1.2,
+  getNumberOfRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  }
+}
+
+let dubaiStore = {
+  min: 11,
+  max: 38,
+  avg: 3.7,
+  getNumberOfRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  }
+}
+
+let parisStore = {
+  min: 20,
+  max: 38,
+  avg: 2.3,
+  getNumberOfRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  }
+}
+
+let seattleObj = {
+  min: 2,
+  max: 16,
+  avg: 4.6,
+  getNumberOfRandomCustomers: function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  }
+}
