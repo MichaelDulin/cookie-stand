3681f4fded8b9f25561b9f2849ad79c224eec95a
MichaelDulin <michaeldulin1211@gmail.com>
2023-01-31 17:39:10 -0800

    Implemented functionality with all store locations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,133 @@
 'use strict';
 
+// Constructor implementation
+// Step 1: setup constructor for dynamic object building
+// Step 2: manipulate DOM into a table instead of list
 
+let storeContainer = document.getElementById('salesTable');
+let storeTBody = document.querySelector('tbody');
+let storeTHead = document.querySelector('thead');
+
+// Constructor
+function Store(name, min, max, avg) {
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  this.id = `${this.name}Profile`;
+  this.getNumberOfRandomCustomers = function() {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  }
+  this.parseArr = function() {
+    let tempArr = [];
+    tempArr[0] = this.name;
+    let cookie = 0;
+    for (let i = 1; i < 15; i++) {
+      cookie = Math.ceil(this.getNumberOfRandomCustomers() * this.avg);
+      tempArr[i] = cookie;
+    }
+    return tempArr;
+  }
+} 
+
+// Handles header
+function headArr() {
+  let hArr = [];
+  let amPm = ':00am';
+  let time = 6;
+  hArr[0] = '';
+  for (let i = 1; i < 15; i++) {
+    if (i == 8) {
+      amPm = ':00pm';
+      time -= 12;
+    }
+    hArr[i] = time + amPm;
+    time += 1;
+  }
+ hArr.push(`Daily Location Total`);
+ renderTHead();
+ function renderTHead() {
+  let tr = document.createElement('td');
+  for (let i = 0; i < hArr.length; i++){
+    let td = document.createElement('td');
+    td.textContent = hArr[i];
+    tr.appendChild(td);
+  }
+  storeTHead.appendChild(tr);
+  //storeContainer.appendChild(storeTHead);
+ }
+}
+
+// Handles footer
+function footArr() {
+  // Provide a row of hourly totals 
+  let fArr = [];
+  fArr[0] = 'Totals';
+}
+
+Store.prototype.renderTable = function() {
+  let tempArr = this.parseArr();
+  let tr = document.createElement('tr');
+  for (let i = 0; i < tempArr.length; i++) {
+    let td = document.createElement('td');
+    td.textContent = tempArr[i];
+    tr.appendChild(td);
+  }
+  tempArr.splice(this.name);
+  storeTBody.appendChild(tr);
+  storeContainer.appendChild(storeTBody);
+
+}
+
+let seattle = new Store(
+  'Seattle',
+  23,
+  65,
+  6.3
+);
+let tokyo = new Store(
+  'Tokyo',
+  3,
+  24,
+  1.2
+);
+let dubai = new Store(
+  'Dubai',
+  11,
+  38,
+  3.7
+);
+let paris = new Store(
+  'Paris',
+  20,
+  38,
+  2.3
+);
+let lima = new Store(
+  'Lima',
+  2,
+  16,
+  4.6
+);
+
+headArr();
+seattle.renderTable();
+tokyo.renderTable();
+dubai.renderTable();
+paris.renderTable();
+lima.renderTable();
+
+
+// Table in HTML 
+/*
+Top row: HTML-<thead> : JS-amPm / time
+Left: 1st column should be store name (seattleArr[0])
+*/
+
+
+
+
+/* Lab6
 let seattleStore = {
   min: 23,
   max: 65,
@@ -52,7 +179,6 @@ let seattleStore = {
     }
   }
 }
-
 let tokyoStore = {
   min: 3,
   max: 24,
@@ -104,7 +230,6 @@ let tokyoStore = {
     }
   }
 }
-
 let dubaiStore = {
   min: 11,
   max: 38,
@@ -156,7 +281,6 @@ let dubaiStore = {
     }
   }
 }
-
 let parisStore = {
   min: 20,
   max: 38,
@@ -208,7 +332,6 @@ let parisStore = {
     }
   }
 }
-
 let limaStore = {
   min: 2,
   max: 16,
@@ -260,10 +383,9 @@ let limaStore = {
     }
   }
 }
-
-
 seattleStore.renderList();
 tokyoStore.renderList();
 dubaiStore.renderList();
 parisStore.renderList();
 limaStore.renderList();
+*/
